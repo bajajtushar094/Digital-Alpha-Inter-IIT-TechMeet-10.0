fd015354e38db9a3e8133cafb9c90fabe214253a
tushar <tbajaj@iitg.ac.in>
2022-03-08 18:25:39 +0530

    deleted migrations and added rquirements.txt

--- backend/dashboard_apis/core/models.py
+++ backend/dashboard_apis/core/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.base_user import BaseUserManager
 from .choices import FILING_TYPES, METRIC_TYPES, METRIC_UNITS
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 
