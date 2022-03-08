286a74f81e8636138fb30966c28915dc2f44fb82
Ankit Guha <ankit11hab@gmail.com>
2022-03-09 00:56:04 +0530

    JWT auth added

--- backend/dashboard_apis/core/urls.py
+++ backend/dashboard_apis/core/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
 from .companies_views import *
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path("companies/bookmark/<str:ticker>", bookmarkCompanyView.as_view())
+    path("companies/bookmark/<str:ticker>", bookmarkCompanyView.as_view()),
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
