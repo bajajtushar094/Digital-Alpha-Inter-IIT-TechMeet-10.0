63ca83b4bd8339f43f7caa8c9680dbb0b83ac5e2
Tushar Bajaj <58565094+bajajtushar094@users.noreply.github.com>
2022-03-12 11:16:10 +0530

    Filter component (#7)
    * added basic fils structure for mainFilter
    
    * added filingtype, ticker and form type in filter
    
    * made further changes
    
    * added mainfilters

--- frontend/dashboard_frontend/src/Components/Widgets/FIlters/TimeFrame/index.jsx
+++ frontend/dashboard_frontend/src/Components/Widgets/FIlters/TimeFrame/index.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import '../../../../global.scss';
 import './timeframe.scss';
 import Calendar from "../../../../images/widgets/Calendar.svg";
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
 const TimeFrame = () => {
     return (
@@ -9,11 +11,11 @@ const TimeFrame = () => {
             <div className="ui-text black100">Timeframe</div>
             <div id="w-node-_41cc1a62-f60f-c68b-ea37-f0b418e4c9f4-5d4911ed" className="filter-ticker">
                 <div id="w-node-ec0d4782-ae1d-f1c1-9d9a-feb852a07851-5d4911ed" className="div-block-13">
-                    <h4 id="w-node-_4a412ca3-f5ce-763d-829c-a0e5771ee42d-5d4911ed" className="black50">From</h4><img src="images/Cross.svg" loading="lazy" alt="" />
+                    <h4 id="w-node-_4a412ca3-f5ce-763d-829c-a0e5771ee42d-5d4911ed" className="black50">From</h4>
                 </div>
                 <div className="tickersearch">
                     <div className="tickerinput">
-                        <h4 className="black50">15 MAR’20</h4>
+                        <DatePicker selected={new Date()} className="black-50"/>
                     </div>
                     <a href="#" className="w-inline-block">
                         <div className="div-block-12"><img src={Calendar} loading="lazy" alt="" /></div>
@@ -22,7 +24,7 @@ const TimeFrame = () => {
             </div>
             <div id="w-node-c071388f-cb3a-5bab-8d80-adf879c84f67-5d4911ed" className="filter-ticker">
                 <div id="w-node-_5800b8ef-4631-a1f8-ce8e-aed70a5571c0-5d4911ed" className="div-block-13">
-                    <h4 id="w-node-_5800b8ef-4631-a1f8-ce8e-aed70a5571c1-5d4911ed" className="black50">To</h4><img src="images/Cross.svg" loading="lazy" alt="" />
+                    <h4 id="w-node-_5800b8ef-4631-a1f8-ce8e-aed70a5571c1-5d4911ed" className="black50">To</h4>
                 </div>
                 <div className="tickersearch">
                     <div className="tickerinput">
