39643f2eea3d7ea3ae9b7c569bb0df53a6060216
Siddhartha kumar singh <siddharthakumarsingh946@gmail.com>
2022-03-13 11:47:45 +0530

    table created 2

--- frontend/dashboard_frontend/src/Pages/Landing/Data.jsx
+++ frontend/dashboard_frontend/src/Pages/Landing/Data.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './data.scss'
+import { useState } from 'react';
 import vector1 from '../../images/nav/Market.svg';
 //componenets
 import PerformerCard from '../../Components/Widgets/PerformerCard'
 import ViewedCard from '../../Components/Widgets/ViewedCard';
+import Table from '../../Components/Widgets/Table';
 
 const Data = () => {
+  const[selected,setsetselected]=useState(true)
   return (
     <div className='landingdata'>
          <div className='top'>
@@ -18,7 +21,12 @@ const Data = () => {
              <ViewedCard />
            </div>
            <div className='tablesec'>
-               Table
+               <div className="table_top">
+                 <button className={selected?'btn-link active':'btn-link'}>Recent fillings</button>
+                 <button className='btn-link'>Bookmarked</button>
+                 <button className='btn-link'>All Companies</button>
+               </div>
+               <Table />
             </div>
          </div>
     </div>
