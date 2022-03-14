19b2e26cd3763d1af57233b8e16bfa3402654453
Tushar Bajaj <58565094+bajajtushar094@users.noreply.github.com>
2022-03-14 12:27:39 +0530

    Company and basket page (#10)
    * added a commit
    
    * added individualBasketPage
    
    * addedother basic components
    
    * added recently viewed

---
+++ frontend/dashboard_frontend/src/Pages/BasketList/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navbar from '../../Components/Global/Navbar/Navbar';
+import RecentlyViewedLogIn from '../../Components/Widgets/RecentlyViewedLogIn/RecentlyViewedLogIn';
+import Watchlist from '../../Components/Widgets/Watchlist';
+import BasketImage from "../../images/widgets/Basket.svg";
+const BasketList = () => {
+    return (
+        <div>
+            <Navbar />
+            <div className="maintitle" style={{display:'flex',flexDirection:'row', justifyContent:'flex-start', justifyItems:'center'}}>
+                <div><img src={BasketImage} height={50} width={50} alt=""/></div>
+                <div style={{display:'flex', flexDirection:'column', justifyContent:'center', justifyItems:'center'}}><h2 className="heading heading-2">WatchLists</h2></div>
+            </div>
+            <div style={{height:"80vh",display:'flex', flexDirection:'column', justifyContent:'space-evenly', justifyItems:'space-around'}}>
+            <Watchlist/>
+            <RecentlyViewedLogIn/>
+            </div>
+        </div>
+    );
+}
+
+export default BasketList;
