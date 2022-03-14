7fd0c6a152a27c149bea6942d20a4d3e2d28f7ee
tushar <tbajaj@iitg.ac.in>
2022-03-14 23:10:48 +0530

    made changes

--- frontend/dashboard_frontend/src/config.js
+++ frontend/dashboard_frontend/src/config.js
@@ -2,8 +2,10 @@ export const LOCAL_SERVER_URL = `http://localhost:8000/`;
 
 export const config = ()=>{
     const confs = {
-        'auth':`${LOCAL_SERVER_URL}auth/token/`
+        'local':{
+            'auth':`${LOCAL_SERVER_URL}auth/token/`
+        }
     }
 
-    return confs;
+    return confs['local'];
 }
