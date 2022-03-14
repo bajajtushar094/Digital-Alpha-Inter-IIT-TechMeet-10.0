f91f96a6ed874db67c5a949dba4beca0062d4af2
tushar <tbajaj@iitg.ac.in>
2022-03-14 12:58:05 +0530

    resolved another confict

--- frontend/dashboard_frontend/src/App.js
+++ frontend/dashboard_frontend/src/App.js
@@ -5,11 +5,8 @@ import Landing from "./Pages/Landing/Landing";
 import Search from "./Pages/Search";
 import IndividualBasket from "./Pages/IndividualBasket";
 import BasketList from "./Pages/BasketList";
-<<<<<<< HEAD
 import Company from "./Pages/Company";
 
-=======
->>>>>>> 19b2e26cd3763d1af57233b8e16bfa3402654453
 
 function App() {
 	return (
@@ -28,12 +25,9 @@ function App() {
 					<Route path='/basketList'>
 						<Route index element={<BasketList/>}/>
 					</Route>
-<<<<<<< HEAD
 					<Route path='/company'>
 						<Route index element={<Company/>}/>
 					</Route>
-=======
->>>>>>> 19b2e26cd3763d1af57233b8e16bfa3402654453
 				</Routes>
 			</BrowserRouter>
 		</div>
