10699e59e32a6dabf525ccd56f1e77d6d09a6e98
Tushar Bajaj <58565094+bajajtushar094@users.noreply.github.com>
2022-03-17 01:14:40 +0530

    watchlist and recent_filings api integrated (#19)
    Co-authored-by: Aman Kumar <79211216+akr-25@users.noreply.github.com>

--- frontend/dashboard_frontend/src/Components/Widgets/RecentlyViewedLogIn/RecentlyViewedLogIn.jsx
+++ frontend/dashboard_frontend/src/Components/Widgets/RecentlyViewedLogIn/RecentlyViewedLogIn.jsx
@@ -1,53 +1,84 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import "../../../global.scss";
 import ASAN from "../../../images/widgets/ASAN.svg";
 import More from "../../../images/widgets/More.svg";
+import { useDispatch } from "react-redux";
+import { getRecentlyViwedCompanies } from "../../../actions/action";
+import { connect } from "react-redux";
+const RecentlyViewedLogIn = (props) => {
+  const dispatch = useDispatch();
+  const data = props.state.recentlyViwedCompanies || [];
+  console.log("Props from RecentlyViewedLogIn;", props);
+  useEffect(() => {
+    async function getRecentViewFunc() {
+      await getRecentlyViwedCompanies(dispatch);
+    }
+    getRecentViewFunc();
+  }, [dispatch]);
 
-const RecentlyViewedLogIn = () => {
-    return (
-        <div class="cardcontainer">
-            <div class="leftcardtitle">
-                <h3 class="heading-2">Recently Viewed</h3>
-                <a href="#" class="button issecondary issmall w-button">view all</a>
-            </div>
-            <div id="w-node-_633401c4-a210-5c3e-5b31-05fd06f3863e-5d4911ed" class="metrics">
-                <div id="w-node-_1e8339b4-def5-75cf-a51f-129473e44da4-5d4911ed" class="listing issmall">
-                    <div class="listingheader-wrapper">
-                        <div class="listingheadergrid">
-                            <div id="w-node-_1e8339b4-def5-75cf-a51f-129473e44da7-5d4911ed" class="compcontainer">
-                                <div class="logo-wrapper"><img src={ASAN} loading="lazy" alt=""/></div>
-                            </div>
-                            <div id="w-node-_1e8339b4-def5-75cf-a51f-129473e44daa-5d4911ed">
-                                <div class="ui-text black100">Asana Ltd.</div>
-                                <h4 class="black15">ASAN</h4>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="actions issmall">
-                        <div class="actioncontainer"></div>
-                        <div class="actioncontainer"><img src={More} loading="lazy" alt=""/></div>
-                    </div>
-                </div>
-                <div id="w-node-_94339718-d38c-804d-a0dd-a845e9df8ee2-5d4911ed" class="listing issmall">
-                    <div class="listingheader-wrapper">
-                        <div class="listingheadergrid">
-                            <div id="w-node-_94339718-d38c-804d-a0dd-a845e9df8ee5-5d4911ed" class="compcontainer">
-                                <div class="logo-wrapper"><img src={ASAN} height={50} width={50} loading="lazy" alt=""/></div>
-                            </div>
-                            <div id="w-node-_94339718-d38c-804d-a0dd-a845e9df8ee8-5d4911ed">
-                                <div class="ui-text black100">Asana Ltd.</div>
-                                <h4 class="black15">ASAN</h4>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="actions issmall">
-                        <div class="actioncontainer"></div>
-                        <div class="actioncontainer"><img src={More} loading="lazy" alt=""/></div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div class='cardcontainer'>
+      <div class='leftcardtitle'>
+        <h3 class='heading-2'>Recently Viewed</h3>
+        <a href='/#' class='button issecondary issmall w-button'>
+          view all
+        </a>
+      </div>
+      <div
+        id='w-node-_633401c4-a210-5c3e-5b31-05fd06f3863e-5d4911ed'
+        class='metrics'
+      >
+        {data.map((el, index) => (
+          <Component
+            key={index}
+            logo={el.logo || ASAN}
+            name={el.name}
+            ticker={el.ticker}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Component = ({logo, name, ticker}) => (
+  <div
+    id='w-node-_1e8339b4-def5-75cf-a51f-129473e44da4-5d4911ed'
+    class='listing issmall'
+  >
+    <div class='listingheader-wrapper'>
+      <div class='listingheadergrid'>
+        <div
+          id='w-node-_1e8339b4-def5-75cf-a51f-129473e44da7-5d4911ed'
+          class='compcontainer'
+        >
+          <div class='logo-wrapper'>
+            <img src={logo} loading='lazy' alt='' />
+          </div>
         </div>
-    );
-}
+        <div id='w-node-_1e8339b4-def5-75cf-a51f-129473e44daa-5d4911ed'>
+          <div class='ui-text black100'>{name}</div>
+          <h4 class='black15'>{ticker}</h4>
+        </div>
+      </div>
+    </div>
+    <div class='actions issmall'>
+      <div class='actioncontainer'></div>
+      <div class='actioncontainer'>
+        <img src={More} loading='lazy' alt='' />
+      </div>
+    </div>
+  </div>
+);
 
-export default RecentlyViewedLogIn;
+// export default RecentlyViewedLogIn;
+// export default Data;
+const mapStateToProps = (state) => {
+	// console.log("State:", state);
+	return {
+		// To get the list of employee details from store
+		state: state,
+	};
+};
+
+export default connect(mapStateToProps, null)(RecentlyViewedLogIn);
