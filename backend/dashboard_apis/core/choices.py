ed4072cd1eaf2440667097d83d850540c3ced2ad
pranavg000 <pranav18@iitg.ac.in>
2022-03-19 20:39:41 +0530

    fixed choices indedatino

--- backend/dashboard_apis/core/choices.py
+++ backend/dashboard_apis/core/choices.py
@@ -1,28 +1,28 @@
 FILING_TYPES = [
-    ('10K', '10K'),
-    ('10Q', '10Q'),
-    ('8K', '8K'),
+	('10K', '10K'),
+	('10Q', '10Q'),
+	('8K', '8K'),
 ]
 
 METRIC_TYPES = [
-  ('Total Revenue','Total Revenue'),
+  	('Total Revenue','Total Revenue'),
 	('ARR', 'Annual Recurring Revenue'),
 	('CCR', 'Customer Churn Rate'),
-  ('MRR','MRR'),
-  ('MRR Expansion','MRR Expansion'),
-  ('ARPU','ARPU'),
+  	('MRR','MRR'),
+  	('MRR Expansion','MRR Expansion'),
+  	('ARPU','ARPU'),
 	('LTV', 'Lifetime Value'),
 	('CAC', 'Customer Acquisition Cost'),
 	('ARPA', 'Average Run Per Account'),
 	('RCC', 'Revenue Churn Rate'),
 	("No Of Customers", "No Of Customers"),
-  ('Churn', 'Churn'),
-  ('MRR Expansion', 'MRR Expansion'),
-  ('Number of qualified leads', 'Number of qualified leads'),
-  ('Penetration Rate', 'Penetration Rate'),
-  ('Sales and Marketing', 'Sales and Marketing'),
-  ('CAC payback', 'CAC payback'),
-  ('Gross Margin', 'ross Margin'),
+  	('Churn', 'Churn'),
+  	('MRR Expansion', 'MRR Expansion'),
+  	('Number of qualified leads', 'Number of qualified leads'),
+  	('Penetration Rate', 'Penetration Rate'),
+  	('Sales and Marketing', 'Sales and Marketing'),
+  	('CAC payback', 'CAC payback'),
+  	('Gross Margin', 'ross Margin'),
 ]
 
 METRIC_UNITS = [
@@ -30,19 +30,19 @@ METRIC_UNITS = [
 	('M', 'Million'),
 	('K', 'Thousand'),
 	('%', 'Percent'),
-  ('USD','USD'),
+	('USD','USD'),
 	('Thousand USD', 'Thousand USD'),
 	('Number', 'Number'),
 ]
 
 SOURCE_TYPES = [
-    ('url', 'URL'), 
-    ('filing', 'Filing')
+	('url', 'URL'), 
+	('filing', 'Filing')
 ]
 
 
 SENTIMENT_LABELS = [
-    ('good', 'good'), 
-    ('bad', 'bad'), 
-    ('medium', 'medium')
+	('good', 'good'), 
+	('bad', 'bad'), 
+	('medium', 'medium')
 ]
