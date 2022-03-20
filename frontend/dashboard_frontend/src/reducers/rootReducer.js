5bfa95a82d92a83504c4bbe2872a1f042e13293f
Aman Kumar <79211216+akr-25@users.noreply.github.com>
2022-03-20 23:52:10 +0530

    date bug fix

--- frontend/dashboard_frontend/src/reducers/rootReducer.js
+++ frontend/dashboard_frontend/src/reducers/rootReducer.js
@@ -19,8 +19,8 @@ const initState = {
         "tickers": [],
         "metric_type": [],
         "form_type":[],
-        "time_start": new Date(new Date().setFullYear(new Date().getFullYear() - 3)),
-        "time_end": new Date()
+        "time_start": startDate,
+        "time_end": endDate
     },
     queryFilingsData: null,
     basketDetails: {
